756cd0ba73317315e928358fac21eb42ccc273ae
Shamil Sadiqov <sov.shamil@gmail.com>
2022-11-02 22:50:03 +0400

    Add missing configuration in setup

--- Imagegram/Program.cs
+++ Imagegram/Program.cs
@@ -1,35 +1,44 @@
+using System.Net;
 using Azure.Storage.Blobs;
+using Imagegram;
 using Imagegram.Database;
-using Imagegram.Features.Posts.Create;
+using Imagegram.ExceptionHandling;
+using Imagegram.Extensions;
+using Imagegram.Features;
+using Imagegram.Features.Posts;
+using Imagegram.Features.Posts.CreatePost.Services;
 using Imagegram.Features.Users;
+using Imagegram.Features.Users.CreateUser;
 using Imagegram.Features.Users.GetUserAccessToken;
+using Imagegram.Features.Users.GetUserAccessToken.Services;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
-// TODO: Move to config
-const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=sovsh;AccountKey=4LzhU4xxGoRxWJ84P9X71WK5DLsoJ8+/FOOdBoIuZsX9F7AVbsaE1ZghIGMjd6XtpAbZurpqqwHT+AStO0eUjA==;EndpointSuffix=core.windows.net";
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// TODO: Extract it in method
 
 var accessTokenOptionsSection = builder.Configuration.GetSection("AccessTokenOptions");
 var accessTokenOptions = accessTokenOptionsSection.Get<AccessTokenOptions>();
-accessTokenOptions.ThrowIfNotValid();
+accessTokenOptions.ThrowIfConfigurationInvalid();
 
-var connectionString = builder.Configuration.GetConnectionString("Default");
+var connectionString = builder.Configuration.GetConnectionString("Database");
+var blobStorageConnectionString = builder.Configuration.GetConnectionString("BlobStorage");
 
 if (string.IsNullOrWhiteSpace(connectionString))
 {
-    throw new InvalidOperationException("Connection string is not provided in config file");
+    throw new ConfigurationException("Connection string for database is not provided in configuration file");
+}
+if (string.IsNullOrWhiteSpace(blobStorageConnectionString))
+{
+    throw new ConfigurationException("Connection string is not provided in config file");
 }
 
 builder.Services
     .AddMediatR(typeof(Program))
-    .AddControllers(x=> x.Filters.Add<ExceptionFilter>())
+    .AddControllers(x => x.Filters.Add<ExceptionFilter>())
     .Services
     .AddEndpointsApiExplorer()
     .AddSwaggerWithAuth()
@@ -37,9 +46,17 @@ builder.Services
     {
         x.Map<EntityNotFoundException>(HttpStatusCode.NotFound);
         x.Map<DuplicateEmailException>(HttpStatusCode.Conflict);
+        x.Map<InvalidPasswordException>(HttpStatusCode.Conflict);
     })
     .Configure<AccessTokenOptions>(accessTokenOptionsSection)
+    .Configure<ImageLimitOptions>(ops =>
+    {
+        // TODO: Extract hardcoded values to config
+        ops.MaxAllowedLength = 104857600;
+        ops.AllowedUploadFormats = new[] { "jpg", "bmp", "png" };
+    })
     .AddSingleton(new BlobServiceClient(blobStorageConnectionString))
+    .AddSingleton<IImageStorage, ImageStorage>()
     .AddSingleton<ImageProcessor>()
     .AddSingleton<IAccessTokenGenerator, AccessTokenGenerator>()
     .AddSingleton<IPasswordManager, PasswordManager>()
@@ -50,31 +67,26 @@ builder.Services
     {
         ops.UseSqlServer(connectionString);
     })
-    .AddAuthentication(ops =>
-    {
-        ops.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        ops.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-        ops.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(ops =>
     {
-        ops.SaveToken = true;
         ops.TokenValidationParameters = new TokenValidationParameters()
         {
             ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(accessTokenOptions.GetSecretBytes()),
-            ValidIssuer = accessTokenOptions.AppName,
-            ValidAudience = accessTokenOptions.AppName,
             ValidateIssuer = true,
             ValidateAudience = true,
+            ValidIssuer = accessTokenOptions.AppName,
+            ValidAudience = accessTokenOptions.AppName,
+            IssuerSigningKey = new SymmetricSecurityKey(accessTokenOptions.GetSecretBytes()),
+            
+            // Not for production
             RequireExpirationTime = false,
-            ValidateLifetime = false,
-            ClockSkew = TimeSpan.Zero
-        };;
+            ValidateLifetime = false 
+        };
     });
 
-
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -85,6 +97,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
